e98a626862811d6aa3e891ecbc541066ea94f194
기남석 <kee0304@naver.com>
2023-02-01 15:37:47 +0900

    [MW] feat STTwithLibrary

--- RM/MW/mirrorweb/src/components/STT.js
+++ RM/MW/mirrorweb/src/components/STT.js
@@ -1,80 +1,50 @@
 import { useEffect, useRef, useState } from "react";
 import { ReactMediaRecorder } from "react-media-recorder";
 import secrets from "../secrets.json"
-import axios from "axios";
+
+import useSpeechToText from "react-hook-speech-to-text"
 
 const STT = () => {
     const API_KEY = secrets.google_speech_api_key
-    const sttURL = `https://speech.googleapis.com/v1p1beta1/speech:recognize?key=${API_KEY}`
-
-    const firMounted = useRef(false);
-    const secMounted = useRef(false);
-
-    const [blobData, setBlob] = useState(null);
-    const [base64Data, setBase64] = useState("")
-
-    useEffect(() => {
-        if (!firMounted.current) {
-            firMounted.current = true;
-        } else {
-            const reader = new FileReader();
-            reader.readAsDataURL(blobData); 
-            reader.onloadend = () => {
-	            const base64data = reader.result;
-	            // base64 converted!
-	            setBase64(base64data.slice(22))
-            }
-        }
-
-    },[blobData])
-
-    useEffect(() => {
-        if (!secMounted.current) {
-          secMounted.current = true;
-        } else {
-          const requestBody = {
-              "audio" : {
-                "content": base64Data
-              },
-              "config": {
-                "enableAutomaticPunctuation": true,
-                "encoding": "LINEAR16",
-                "languageCode": "ko-KR",
-                "model": "default"
-              }
-            }
-            console.log(base64Data)
-          axios.post(sttURL, requestBody)
-          .then((response) => {
-            console.log(response)
+    const {
+        error,
+        interimResult,
+        isRecording,
+        results,
+        startSpeechToText,
+        stopSpeechToText,
+      } = useSpeechToText({
+        continuous: true,
+        useLegacyResults: false,
+        useOnlyGoogleCloud: true,
+        googleApiKey: API_KEY,
+        googleCloudRecognitionConfig: {
+            languageCode: 'ko-KR'
+          }
+      });
     
-          })
-          .catch((err) => {
-              console.log(err)
-          })
-        }
-      },[base64Data])
+    useEffect(() => {
+        console.log(results)
+    },[results])
 
-  return (
-    <div>
-    <ReactMediaRecorder
-      audio
-      onStop={(blobUrl, blob) => {  
-        console.log(blob);
-        setBlob(blob)
-    }}
-      render={({ status, startRecording, stopRecording, mediaBlobUrl, clearBlobUrl }) => (
+      if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
+
+      return (
         <div>
-          <p>{status}</p>
-          <button onClick={startRecording}>Start Recording</button>
-          <button onClick={stopRecording}>Stop Recording</button>
-          <button onClick={clearBlobUrl}>Clear Blob</button>
-          <audio src={mediaBlobUrl} controls autoPlay loop />
+          <h1>Recording: {isRecording.toString()}</h1>
+          <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
+            {isRecording ? 'Stop Recording' : 'Start Recording'}
+          </button>
+          <ul>
+            {results.map((result) => (
+              <li key={result.timestamp}>{result.transcript}</li>
+            ))}
+            {interimResult && <li>{interimResult}</li>}
+          </ul>
         </div>
-      )}
-    />
-  </div>
-  );
-};
+      );
+    }
+
+
 
 export default STT;
